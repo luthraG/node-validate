18038ef6c769d609ab0ad2debde993578a0afa2d
Gaurav Luthra <luthra.zenith@gmail.com>
2017-05-28 09:23:13 +0100

    Updated tests for contains validation API

--- test/contains.js
+++ test/contains.js
@@ -10,46 +10,46 @@ describe('contains API Tests', function () {
             expect(contains(baseStr, searchStr)).to.be.true;
         });
 
-        baseStr = 'GAURAV IS AWESOME';
-        searchStr = 'awesome';
+        var baseStr1 = 'GAURAV IS AWESOME';
+        var searchStr1 = 'awesome';
 
         it('contains value test 2', function () {
-            expect(contains(baseStr, searchStr)).to.be.true;
+            expect(contains(baseStr1, searchStr1)).to.be.false;
         });
 
-        baseStr = 'awesome';
-        searchStr = 'awe';
+        var baseStr2 = 'awesome';
+        var searchStr2 = 'awe';
 
         it('contains value test 3', function () {
-            expect(contains(baseStr, searchStr)).to.be.true;
+            expect(contains(baseStr2, searchStr2)).to.be.true;
         });
 
-        baseStr = 'This is an epic line';
-        searchStr = 'epic';
+        var baseStr3 = 'This is an epic line';
+        var searchStr3 = 'epic';
 
         it('contains value test 4', function () {
-            expect(contains(baseStr, searchStr)).to.be.true;
+            expect(contains(baseStr3, searchStr3)).to.be.true;
         });
 
-        baseStr = 'null';
-        searchStr = 'null';
+        var baseStr4 = 'null';
+        var searchStr4 = 'null';
 
         it('contains value test 5', function () {
-            expect(contains(baseStr, searchStr)).to.be.true;
+            expect(contains(baseStr4, searchStr4)).to.be.true;
         });
 
-        baseStr = 'undefined is not null';
-        searchStr = 'null';
+        var baseStr5 = 'undefined is not null';
+        var searchStr5 = 'null';
 
         it('contains value test 6', function () {
-            expect(contains(baseStr, searchStr)).to.be.true;
+            expect(contains(baseStr5, searchStr5)).to.be.true;
         });
 
-        baseStr = 'GAURAV IS AWESOME';
-        searchStr = 'AWESOME';
+        var baseStr6 = 'GAURAV IS AWESOME';
+        var searchStr6 = 'AWESOME';
 
         it('contains value test 7', function () {
-            expect(contains(baseStr, searchStr)).to.be.true;
+            expect(contains(baseStr6, searchStr6)).to.be.true;
         });
     });
 
